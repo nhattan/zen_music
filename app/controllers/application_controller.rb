616d439d0acf94eb9defa27dc526e4c95e9489fe
Tan <nhattan2012@gmail.com>
2016-08-05 23:30:13 +0700

    permit addition attributes in devise actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,5 +10,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:phone])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:phone, :name, :image])
   end
 end
