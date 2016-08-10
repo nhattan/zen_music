3c6e725eb939aeb400d540025d3ad9781c4358c2
Tan <nhattan2012@gmail.com>
2016-08-10 08:23:48 +0700

    devise views

--- app/assets/javascripts/admin/application.js
+++ app/assets/javascripts/admin/application.js
@@ -13,4 +13,6 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require jquery_ujs
+//= require app
+//= require jquery-slimscroll
 //= require_tree .
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   # token auth routes available at /api/v1/auth
   namespace :api do
     scope :v1 do
-      mount_devise_token_auth_for 'User', at: 'auth'
+      mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
     end
   end
 
