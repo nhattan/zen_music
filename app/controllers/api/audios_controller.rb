9389c36472d67d8fb7f32ffbaf8f02ce726594f4
Tan <nhattan2012@gmail.com>
2016-08-29 18:02:54 +0700

    includes category to avoid N + 1 queries

--- app/controllers/api/audios_controller.rb
+++ app/controllers/api/audios_controller.rb
@@ -1,6 +1,6 @@
 class Api::AudiosController < Api::ApplicationController
   def index
-    @audios = Audio.approved
+    @audios = Audio.approved.includes(:category)
   end
 
   def show
@@ -9,10 +9,10 @@ class Api::AudiosController < Api::ApplicationController
   end
 
   def top
-    @audios = Audio.top.limit(Setting.top_audio_quantity)
+    @audios = Audio.includes(:category).top.limit(Setting.top_audio_quantity)
   end
 
   def favorite
-    @audios = current_user.favorite_audios.order(created_at: :desc)
+    @audios = current_user.favorite_audios.includes(:category).order(created_at: :desc)
   end
 end
